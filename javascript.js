6e9e1ab9ad39d559dc6863893c3e635f8c51eeac
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-06 17:14:17 +0400

    Added change size of squares

--- javascript.js
+++ javascript.js
@@ -1,25 +1,37 @@
 const contDiv = document.querySelector("#container");
 const squareNum = document.querySelector("#square-number");
-let squares;
+let squares = 256;
+const heading2 = document.querySelector(".heading-2");
+
+createSquares(squares);
 
 function getSquares(n){
   return n * n;
 }
 
+squareNum.addEventListener("click", () => {
+  let square = prompt("Enter how many squares you want", "0");
+  
+  if (square >= 0 && square !== null && square <= 100){
+    for(let j = 0; j < squares; j++){    
+      contDiv.removeChild(contDiv.firstChild);    
+    };
+    squares = getSquares(square);  
+    createSquares(squares);  
+    heading2.textContent = `${square} x ${square}`;
+  }
+}); 
 
-for(let i = 0; i < 257; i++){
-  const div = document.createElement("div");
-  div.style.width = "20px";
-  div.style.height = "20px";
-  div.style.border = "1px solid black";
 
-  contDiv.appendChild(div);
-  div.addEventListener("mouseover", () => {    
-    div.classList.add("hover");     
-  });
+function createSquares (n){  
+  for(let i = 0; i < n; i++){
+    const div = document.createElement("div");
+    div.style.width = "100%";
+    div.style.height = "100%";    
+    div.style.border = "1px solid black"; 
+    contDiv.appendChild(div);
+    div.addEventListener("mouseover", () => {    
+      div.classList.add("hover");     
+    });
+  }  
 }
-
-squareNum.add("click", () => {
-  squares = alert("Enter how many squares you want", 0);
-})
-
