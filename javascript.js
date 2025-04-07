f37f3e5b3dd0fb65c170583174dc3d29df96c1bc
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-08 00:00:20 +0400

    Added on click start coloring Added on click start coloring and click second time it stops. also added reset button without functionality, changed borders from dotted to solid

--- javascript.js
+++ javascript.js
@@ -4,13 +4,16 @@ const heading2 = document.querySelector(".heading-2");
 const darkButton = document.querySelector("#dark");
 const lightButton = document.querySelector("#light");
 let squares = 256;
+let isClicked = false;
+let divs;
+
 function getSquares(n) {
   return n * n;
 }
 
 squareNum.addEventListener("click", () => {
-  let square = prompt("Enter how many squares you want", "0");
-  if (square >= 0 && square !== null && square <= 100) {
+  let square = prompt("Enter how many squares you want from 1 to 100", "1");
+  if (square > 0 && square !== null && square <= 1000) {
     for (let j = 0; j < squares; j++) {
       contDiv.removeChild(contDiv.firstChild);
     }
@@ -34,13 +37,32 @@ function createSquares(squares, square) {
     const div = document.createElement("div");
     div.style.height = `${500 / square}px`;
     div.style.width = `${500 / square}px`;
-    div.style.border = "1px dotted black";
+    div.style.border = "1px solid black";
     div.style.boxSizing = "border-box";
     contDiv.appendChild(div);
-    div.addEventListener("mouseover", (e) => {
-      div.style.backgroundColor = getRandomColor();
-    });
   }
+}
+
+
+contDiv.addEventListener("click", () => {
+  if (!isClicked){
+    divs = contDiv.querySelectorAll("div");
+    divs.forEach((div) => {
+      div.addEventListener("mousemove", coloring);
+    });
+    isClicked = true;
+  } else if (isClicked){
+    divs = contDiv.querySelectorAll("div");
+    divs.forEach((div) => {
+    div.removeEventListener("mousemove", coloring);
+  });
+  isClicked = false;
+  }
+});
+
+function coloring(e) {
+  e.target.style.backgroundColor = getRandomColor();
+  // e.target.style.backgroundColor = "black";
 }
 
 createSquares(squares, 16);
